9d942525bafba07557a9f0a79d062deffee0129d
peterjmg <peter.glennane@iname.com>
2014-11-04 13:15:30 +0000

    Added caching using Guava cache
    Missing file

--- src/main/java/primes/config/WebConfig.java
+++ src/main/java/primes/config/WebConfig.java
@@ -1,11 +1,20 @@
 package primes.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan(basePackages = {"primes.controller"})
 @EnableWebMvc
+@PropertySource({ "classpath:primes.properties" })
 public class WebConfig {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer properties() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 }
